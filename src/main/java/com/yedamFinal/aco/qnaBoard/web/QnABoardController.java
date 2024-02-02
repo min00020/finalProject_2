475472e1769acf4098f10991dbb2425023dc2574
minky22 <c11286@naver.com>
2024-02-02 14:50:34 +0900

    f

--- src/main/java/com/yedamFinal/aco/qnaBoard/web/QnABoardController.java
+++ src/main/java/com/yedamFinal/aco/qnaBoard/web/QnABoardController.java
@@ -2,6 +2,8 @@ package com.yedamFinal.aco.qnaBoard.web;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,14 +18,12 @@ import com.yedamFinal.aco.qnaBoard.serviceImpl.QnABoardServiceImpl;
 
 @Controller
 public class QnABoardController {
-	
 	@Autowired
 	private QnABoardServiceImpl qnaBoardService;
 	
-	// min 문의게시판 리스트 불러오기(페이징, 검색등)
+	// min 문의게시판 리스트 불러오기(페이징, 검색, 정렬등)
 	@GetMapping("/qnaBoard")
-	public String getQnaBoardListForm(@RequestParam String pg, String search, String ob, Model model) {
-		// MemberVO 꺼내오기.
+	public String getQnaBoardListForm(@RequestParam String pg, String search, String ob, HttpServletRequest req, Model model) {// MemberVO 꺼내오기.
 		MemberVO vo = null;
 		Map<String,Object> ret = null;
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -36,7 +36,7 @@ public class QnABoardController {
             
             // 검색창 입력의 경우.
             if(search == null)
-            	ret = qnaBoardService.getMyQnaBoardList(Integer.valueOf(pg), vo,ob);
+            	ret = qnaBoardService.getMyQnaBoardList(Integer.valueOf(pg), vo, ob);
             else {
             	ret = qnaBoardService.getMyQnaBoardListFromSearch(Integer.valueOf(pg), search, vo,ob);
             	model.addAttribute("search",search); // 해당 search키워드로 페이징해야함.
