87672955939a49035ec5bfca9bcfe35fe6ade52d
minky22 <c11286@naver.com>
2024-02-13 17:06:25 +0900

    hi

--- src/main/java/com/yedamFinal/aco/common/web/CommonController.java
+++ src/main/java/com/yedamFinal/aco/common/web/CommonController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartRequest;
 
 import com.yedamFinal.aco.common.AttachedFileVO;
 import com.yedamFinal.aco.common.serviceImpl.FileServiceImpl;
--- src/main/java/com/yedamFinal/aco/qnaBoard/web/QnABoardController.java
+++ src/main/java/com/yedamFinal/aco/qnaBoard/web/QnABoardController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
