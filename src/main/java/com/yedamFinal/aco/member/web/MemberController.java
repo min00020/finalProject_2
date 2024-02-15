8c8bd7179d4a7c6982ae277ab9b66706a82758a6
= <c11286@naver.com>
2024-02-16 02:12:08 +0900

    profile

--- src/main/java/com/yedamFinal/aco/member/web/MemberController.java
+++ src/main/java/com/yedamFinal/aco/member/web/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -330,6 +331,12 @@ public class MemberController {
 		}
 		
 		return memberService.changePasswordFromMyPage(password, passwordVerify, memberVO.getId());
+	}
+	
+	@GetMapping("/member/{mno}")
+	public String getMemberProfileInfo(@PathVariable("mno") int memberNo) {
+		
+		return "common/memberProfile";
 	}
 	
 }
